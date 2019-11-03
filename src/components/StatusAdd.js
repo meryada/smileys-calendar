c67e9f99242536b5c34b680714a124d272aa75fb
María Mata <matah.maria@gmail.com>
2019-11-03 17:21:58 +0100

    central column to mixin > added min width

--- src/components/StatusAdd.js
+++ src/components/StatusAdd.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import '../styles/StatusAdd.scss';
 
 const StatusAdd = props => {
-    return (
-        <Link to='/form'>
-        <div className='add__container'>
-        <h2 className='add__title'> + :D</h2>
+  return (
+    <Link to='/form'>
+      <div className='add__container'>
+        <div className='add__btn'>
+          <h2 className='add__title'>+</h2>
         </div>
-        </Link>
-    )
+      </div>
+    </Link>
+  )
 };
 
 export { StatusAdd };
