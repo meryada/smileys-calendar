7435097a871521a7c9dc7fe5b0d4f0312d938278
María Mata <matah.maria@gmail.com>
2019-11-04 02:24:35 +0100

    some styles to status details

--- src/components/StatusDetail.js
+++ src/components/StatusDetail.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/StatusDetail.scss';
 
 const StatusDetail = props => {
-  const {routerProps, allStatus} = props
+  const { routerProps, allStatus } = props
   const id = routerProps.match.params.id;
   const state = allStatus.filter(item => item.newId == id);
-  if(state[0]) {
+  if (state[0]) {
     return (
       <div className='detail__container'>
         <p>ID/#{id}</p>
-      <h2>{state[0].currentDay}</h2>
-      <p>{state[0].currentStatus === 'triste'?<i className="fas fa-sad-tear"></i>:<i className="fas fa-grin-hearts"></i>}</p>
-      <p>{state[0].currentMessage}</p>
-      <Link to="/">
-      <button type='button' value='Cancelar'>Volver al listado</button>
-    </Link>
-    </div>
-  )
+        <h2>{state[0].currentDay}</h2>
+        <p>{state[0].currentStatus === 'triste' ? <i className="fas fa-sad-tear"></i> : <i className="fas fa-grin-hearts"></i>}</p>
+        <p>{state[0].currentMessage}</p>
+        <Link to="/">
+          <button type='button' value='Cancelar'>Volver al listado</button>
+        </Link>
+      </div>
+    )
   } else {
-    return(
-      <p>este stado no exite</p>
+    return (
+      <div>
+        <p>este stado no exite</p>
+        <Link to="/">
+          <button type='button' value='Cancelar'>Volver al listado</button>
+        </Link>
+      </div>
     )
   }
 };
